4e70e7e4f06019028a685a96305058b12f5dbcc3
Pawel Josefsson <adminuser@zbook>
2022-11-11 21:43:04 +0100

    Use Ubuntu

--- tf/main.tf
+++ tf/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     vultr = {
       source = "vultr/vultr"
-      version = "2.11.4"
+      version = "~> 2.0"
     }
   }
 }
@@ -11,6 +11,9 @@ terraform {
 provider "vultr" {
   # In your .bashrc you need to set
   # export VULTR_API_KEY="Your Vultr API Key"
+  # 
+  # https://www.vultr.com/api/
+  #
 }
 
 resource "vultr_instance" "my_instance" {
@@ -22,30 +25,30 @@ resource "vultr_instance" "my_instance" {
   backups                = "disabled"
   activation_email       = false
   ddos_protection        = false
-  tag                    = "tag"
-  ssh_key_ids            = [ vultr_ssh_key.my_ssh_key.id ]
+  tags                    = [ "tag" ]
+  ssh_key_ids            = [ vultr_ssh_key.tf_ssh_key.id ]
 
   provisioner "remote-exec" {
     # Install ansible to get all correct python configuration
-    inline = [ "sudo apt-get update", "sudo apt-get install ansible -y", "echo Done!"]
+    inline = [ "sudo apt update", "echo Done!"]
 
     connection {
       host        = self.main_ip
       type        = "ssh"
       user        = "root"
-      private_key = chomp(file("~/.ssh/id_rsa"))
+      private_key = chomp(file("/var/lib/ssh/id_ed25519"))
     }
   }
 
-  provisioner "local-exec" {
-    command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.main_ip},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' ansible.yml"
-  }
+  #provisioner "local-exec" {
+  #  command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -u root -i '${self.main_ip},' --private-key ${var.pvt_key} -e 'pub_key=${var.pub_key}' ansible.yml"
+  #}
 
 }
 
-resource "vultr_ssh_key" "my_ssh_key" {
-  name = "my-ssh-key"
-  ssh_key = chomp(file("~/.ssh/id_rsa.pub"))
+resource "vultr_ssh_key" "tf_ssh_key" {
+  name = "tf_ssh_key"
+  ssh_key = chomp(file("/var/lib/ssh/id_ed25519.pub"))
 }
 
 
--- tf/variable.tf
+++ tf/variable.tf
@@ -1,24 +1,33 @@
+# 
+# https://www.vultr.com/api/
+#
+
+
 variable "vultr_region" {
   description = "Vultr Frankfurt Region"
-  default = "fra"
+  default = "sto"
 }
 
+# curl "https://api.vultr.com/v2/os" -X GET   -H "Authorization: Bearer ${VULTR_API_KEY}" | jq
 variable "os" {
-  description = "Ubuntu 18.04"
-  default = 270
+  description = "Fedora 37 x64"
+  default = 1946
+#  default = 1929
 }
 
+# curl "https://api.vultr.com/v2/plans"   -X GET   -H "Authorization: Bearer ${VULTR_API_KEY}" | jq
 variable "one_cpu_one_gb_ram" {
   description = "1024 MB RAM,25 GB SSD,1.00 TB BW"
-  default = "vc2-1c-1gb"
+  default = "vc2-6c-16gb"
+  #default = "vc2-1c-1gb"
 }
 
 variable "pub_key" {
   description = "Public SSH key"
-  default = "~/.ssh/id_rsa.pub"
+  default = "/var/lib/ssh/id_ed25519.pub"
 }
 
 variable "pvt_key" {
   description = "Private SSH key"
-  default = "~/.ssh/id_rsa"
+  default = "/var/lib/ssh/id_ed25519"
 }
